172cde9367cf70f79dc1eb0bcaf8401ca4a6ac89
dataz <dataz.karas@gmail.com>
2020-02-27 19:20:50 +0000

    Migration fix

--- src/Migrations/Version20200227144928.php
+++ src/Migrations/Version20200227144928.php
@@ -22,9 +22,9 @@ final class Version20200227144928 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE trip_measures (id INT AUTO_INCREMENT NOT NULL, trip_id_id INT NOT NULL, distance NUMERIC(5, 2) NOT NULL, INDEX IDX_F8DB2F45A50F1E14 (trip_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE trip_measures (id INT AUTO_INCREMENT NOT NULL, trip_id INT NOT NULL, distance NUMERIC(5, 2) NOT NULL, INDEX IDX_F8DB2F45A50F1E14 (trip_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE trips (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(20) NOT NULL, measure_interval INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE trip_measures ADD CONSTRAINT FK_F8DB2F45A50F1E14 FOREIGN KEY (trip_id_id) REFERENCES trips (id)');
+        $this->addSql('ALTER TABLE trip_measures ADD CONSTRAINT FK_F8DB2F45A50F1E14 FOREIGN KEY (trip_id) REFERENCES trips (id)');
     }
 
     public function down(Schema $schema) : void
