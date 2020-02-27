2420457a67148eeeef3d24a0fa2f8a2aa09a0923
dataz <dataz.karas@gmail.com>
2020-02-27 15:15:19 +0000

    Fixtures & yaml

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\TripMeasures;
+use App\Entity\Trips;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
+
+class AppFixtures extends Fixture
+{
+	private $tripsStorage = __DIR__.'/../../config/trips.yaml';
+	private function provideInitialData()
+	{
+		try
+		{
+			if(file_exists($this->tripsStorage))
+			{
+				return Yaml::parseFile($this->tripsStorage);
+			}
+			throw new ParseException('File doesn\'t exist');
+		}
+		catch (ParseException $exception)
+		{
+			printf('Unable to parse the YAML string: %s', $exception->getMessage());
+		}
+	}
+	private function handleSingleTrip(array $singleTrip) : \App\Entity\Trips
+	{
+		$trip = new Trips();
+		$trip->setName($singleTrip['name']);
+		$trip->setMeasureInterval($singleTrip['measure_interval']);
+		return $trip;
+	}
+	private function handleTripMeasure(float $distances, \App\Entity\Trips $tripEntity)
+	{
+		$measure = new TripMeasures();
+		$measure->setDistance($distances);
+		$measure->setTripId($tripEntity);
+		return $measure;
+	}
+	public function load(ObjectManager $manager)
+	{ 
+		$tripsdata = $this->provideInitialData();
+		if($tripsdata)
+		{
+			foreach($tripsdata as $singleTrip)
+			{
+				$tripEntity = $this->handleSingleTrip($singleTrip);
+				$manager->persist($tripEntity);
+				foreach($singleTrip['distances'] as $distance)
+				{
+					$manager->persist($this->handleTripMeasure($distance, $tripEntity));
+				}
+				
+			}
+		}
+		$manager->flush();
+	}
+}
---
+++ src/Migrations/Version20200227144928.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200227144928 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE trip_measures (id INT AUTO_INCREMENT NOT NULL, trip_id_id INT NOT NULL, distance NUMERIC(5, 2) NOT NULL, INDEX IDX_F8DB2F45A50F1E14 (trip_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE trips (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(20) NOT NULL, measure_interval INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE trip_measures ADD CONSTRAINT FK_F8DB2F45A50F1E14 FOREIGN KEY (trip_id_id) REFERENCES trips (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE trip_measures DROP FOREIGN KEY FK_F8DB2F45A50F1E14');
+        $this->addSql('DROP TABLE trip_measures');
+        $this->addSql('DROP TABLE trips');
+    }
+}
